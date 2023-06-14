13f7c0f0ab285209bf760ef38fa14d5473903d66
Ardhendu100 <ardhendusekharsahoo37@gmail.com>
2023-06-14 23:51:44 +0530

    fixed ui issue

--- resources/views/vendor/laravel-filemanager/index.blade.php
+++ resources/views/vendor/laravel-filemanager/index.blade.php
@@ -36,17 +36,17 @@
     <a class="navbar-brand d-block d-lg-none" id="current_folder"></a>
     <a id="loading" class="navbar-brand"><i class="fas fa-spinner fa-spin"></i></a>
     <div class="ml-auto px-2">
-      <a class="navbar-link d-none" id="multi_selection_toggle">
+      <!-- <a class="navbar-link d-none" id="multi_selection_toggle">
         <i class="fa fa-check-double fa-fw"></i>
         <span class="d-none d-lg-inline">{{ trans('laravel-filemanager::lfm.menu-multiple') }}</span>
-      </a>
+      </a> -->
     </div>
     <a class="navbar-toggler collapsed border-0 px-1 py-2 m-0" data-toggle="collapse" data-target="#nav-buttons">
       <i class="fas fa-cog fa-fw"></i>
     </a>
     <div class="collapse navbar-collapse flex-grow-0" id="nav-buttons">
       <ul class="navbar-nav">
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link" data-display="grid">
             <i class="fas fa-th-large fa-fw"></i>
             <span>{{ trans('laravel-filemanager::lfm.nav-thumbnails') }}</span>
@@ -63,16 +63,16 @@
             <i class="fas fa-sort fa-fw"></i>{{ trans('laravel-filemanager::lfm.nav-sort') }}
           </a>
           <div class="dropdown-menu dropdown-menu-right border-0"></div>
-        </li>
+        </li> -->
       </ul>
     </div>
   </nav>
-
+<!-- 
   <nav class="bg-light fixed-bottom border-top d-none" id="actions">
     <a data-action="open" data-multiple="false"><i class="fas fa-folder-open"></i>{{ trans('laravel-filemanager::lfm.btn-open') }}</a>
     <a data-action="preview" data-multiple="true"><i class="fas fa-images"></i>{{ trans('laravel-filemanager::lfm.menu-view') }}</a>
     <a data-action="use" data-multiple="true"><i class="fas fa-check"></i>{{ trans('laravel-filemanager::lfm.btn-confirm') }}</a>
-  </nav>
+  </nav> -->
 
   <div class="d-flex flex-row">
     <div id="tree"></div>
@@ -230,18 +230,18 @@
         label: lang['menu-move'],
         multiple: true
       },
-      {
-        name: 'resize',
-        icon: 'arrows-alt',
-        label: lang['menu-resize'],
-        multiple: false
-      },
-      {
-        name: 'crop',
-        icon: 'crop',
-        label: lang['menu-crop'],
-        multiple: false
-      },
+      // {
+      //   name: 'resize',
+      //   icon: 'arrows-alt',
+      //   label: lang['menu-resize'],
+      //   multiple: false
+      // },
+      // {
+      //   name: 'crop',
+      //   icon: 'crop',
+      //   label: lang['menu-crop'],
+      //   multiple: false
+      // },
       {
         name: 'trash',
         icon: 'trash',
